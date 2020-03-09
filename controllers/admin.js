7457655a07e1cf53e677ebadd87be6e3de3adff2
LJaglin <ljaglinski@o2.pl>
2020-03-09 20:59:53 +0100

    Some fake data in response

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,6 @@
 exports.getUsers = (req, res, next) => {
     console.log('getUsers');
+    res.status(200).json({
+        users: [{firstName: 'John', lastName: 'Doe'}, {firstName: 'James', lastName: 'Bond'}]
+    });
 };
