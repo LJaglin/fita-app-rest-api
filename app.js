96772ef1314f1cc07e269a0efef28ac3391b05b1
LJaglin <ljaglinski@o2.pl>
2020-04-08 20:20:42 +0200

    Fix problem with CORS error

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ app.use(bodyParser.json());
 //CORS errors
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Header', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE', 'OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
 
